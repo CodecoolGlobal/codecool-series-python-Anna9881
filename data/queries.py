290665850d77af1bb7158ad54ac5d14de5251f56
Anna Wysocka <wysocka781@gmail.com>
2024-02-24 15:46:55 +0100

    shows_most_rated_done

--- data/queries.py
+++ data/queries.py
@@ -8,7 +8,7 @@ def get_shows():
 def get_most_rated_shows():
     return data_manager.execute_select(
         """ 
-       SELECT title, year, runtime, trailer, rating, STRING_AGG(genres.name, ', ') AS genres FROM shows
+       SELECT title, TO_CHAR(year, 'YYYY') AS year, runtime, COALESCE(trailer, 'No URL') AS trailer, ROUND(rating, 2) AS rating, STRING_AGG(genres.name, ', ') AS genres, homepage FROM shows
         LEFT JOIN show_genres ON shows.id = show_genres.show_id
         LEFT JOIN genres ON genres.id = show_genres.genre_id
         GROUP BY shows.id
